93d8a4a05ec0585829270cb894ea06f1b644e648
Adam Sayer <asayer82@icloud.com>
2024-11-28 12:39:05 +1100

    Initial Commit

---
+++ Historical Sydney/Historical_SydneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Historical_SydneyApp.swift
+//  Historical Sydney
+//
+//  Created by Adam on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Historical_SydneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
